3bee077fe3cb5359ef9e7c3e397a90fb090f7e48
RakhiManoj <55633116+RakhiManoj@users.noreply.github.com>
2019-09-23 15:43:19 +0530

    second commit to git

---
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -0,0 +1,17 @@
+package com.faith.test;
+
+public class Main {
+	
+ public static void main(String [] args){
+	 
+	 SalesData objSalesData=new SalesData();
+	 System.out.println("hello ust guys!!!");
+	 displayGreetings();
+	 objSalesData.display();
+	 
+ }
+ 
+ private static void displayGreetings(){
+	 System.out.println("welcome to sales report app 2019.....");
+ }
+}
